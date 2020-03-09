3a36b23ddc35236208874b202bffdc22adef7157
Allison Winter-Reed <38668004+AllisonWinter-Reed@users.noreply.github.com>
2020-03-08 19:01:29 -0500

    Milestone 1 complete -Note: there was an issue loading the map but it was fixed by enabling the MAPS SDK for Android from the google cloud services

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.android.lab_6;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Somewhere in Australia
+    private final LatLng mDestinationLatLng = new LatLng(-33.852341, 151.2106085);
+    private final LatLng bascomHall = new LatLng(43.0753078,-89.4216763);
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions().position(bascomHall).title("Bascom Hall"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(bascomHall));
+        });
     }
 }
